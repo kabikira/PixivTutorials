1fb8101b1de7b6269dce29f7497a77250917b3ad
kabikira <kabikirar2@protonmail.com>
2024-04-28 12:39:21 +0900

    first commit

---
+++ PixivTutorials1/IllustCell.swift
@@ -0,0 +1,17 @@
+//
+//  IllustCell.swift
+//  PixivTutorials1
+//
+//  Created by koala panda on 2024/04/28.
+//
+
+import UIKit
+
+class IllustCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .red
+    }
+
+}
